c8b52ad3a25826aa254f71b411b514c725f8bba0
arpana31 <83693542+arpana31@users.noreply.github.com>
2023-01-08 14:24:08 +0530

    Add files via upload
    initial commit

---
+++ AWS/src/com/jala/Helloworld.java
@@ -0,0 +1,13 @@
+package com.jala;
+
+public class Helloworld {
+	 
+	    public static void main(String[] args) {
+	        String name = "jala";
+	        /* The System.out.print() is a very frequently used method
+	            for printing to the console or the standard output. */
+	        System.out.println("My name is " + name);
+	    }
+	}
+
+
